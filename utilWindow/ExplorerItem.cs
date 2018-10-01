2e93d4a1615e2bb35a198aa27e9a1a2a0ce74c02
sh1so6 <jade.shisonoha@gmail.com>
2018-10-01 11:17:18 +0900

    add : OmniWindowの機能を移植 {  add : Alt+Q キーによる呼び出し  add : Explorerのハンドルとファイルパス取得 }

---
+++ utilWindow/ExplorerItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utilWindow
+{
+	class ExplorerItem
+	{
+		public int handle { get; set; }
+		public string path { get; set; }
+	}
+}
--- utilWindow/MainWindow.xaml.cs
+++ utilWindow/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HongliangSoft.Utilities.Gui;
+using Shell32;
+using SHDocVw;
+using System.IO;
+using System.Collections.ObjectModel;
 
 namespace utilWindow
 {
@@ -20,9 +25,56 @@ namespace utilWindow
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private KeyboardHook KeyboardHook;
+		private ObservableCollection<ExplorerItem> Data { get; } = new ObservableCollection<ExplorerItem>();
 		public MainWindow()
 		{
 			InitializeComponent();
+			KeyboardHook = new KeyboardHook(CallMainWindow);
+
+			Shell shell = new Shell();
+			ShellWindows shellWindows = new ShellWindows();
+			foreach (InternetExplorer ieObj in shellWindows)
+			{
+				//エクスプローラのみ(IEを除外)
+				if (System.IO.Path.GetFileName(ieObj.FullName).ToUpper() == "EXPLORER.EXE")
+				{
+					//パスを取得できなかったものを除外
+					if (ieObj.LocationURL != "")
+					{
+						Data.Add(new ExplorerItem { handle = ieObj.HWND, path = new Uri(ieObj.LocationURL).LocalPath + " : " + ieObj.LocationName });
+					}
+
+				}
+			}
+			this.GridExplorer.ItemsSource = Data;
+		}
+		/// <summary>
+		/// globalHookを実現する自作イベントハンドラ
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void CallMainWindow(object sender, HongliangSoft.Utilities.Gui.KeyboardHookedEventArgs e)
+		{
+			if (e.UpDown != KeyboardUpDown.Down)
+			{
+				if (e.AltDown && e.KeyCode == System.Windows.Forms.Keys.Q)
+				{
+					if (!this.IsVisible)
+					{
+						this.Show();
+					}
+
+					if (this.WindowState == WindowState.Minimized)
+					{
+						this.WindowState = WindowState.Normal;
+					}
+					this.Activate();
+					this.Topmost = true;  // important
+					this.Topmost = false; // important
+					this.Focus();         // important
+				}
+			}
 		}
 	}
 }
